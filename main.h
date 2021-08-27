d73f36a9ccd97c82bec14bc56bbf0bf3a042a4a4
yurii <76736848+Hackerman20334@users.noreply.github.com>
2021-08-27 03:06:35 +0300

    Add files via upload

---
+++ main.h
@@ -0,0 +1,11 @@
+#define CM_FILE_SAVEAS	9072
+#define CM_FILE_EXIT	9071
+#define CM_FILE_OPEN	9070
+#define CM_ABOUT        9069
+#define CM_GIT          9068
+#define CM_START        9067
+#define CM_VER          9066
+#define CM_NEW          9065
+#define CM_HELP         9064
+
+#define IDC_MAIN_TEXT 1001
